416389451b90524a738632ffc5b989b2e84b5c65
pledac <pierre.ledac@cea.fr>
2026-02-23 16:57:13 +0100

    [ROCm] Unexplained fix to avoid crash (if several nodes used) on kernels using ParserView

--- src/Kernel/Math/ParserView.h
+++ src/Kernel/Math/ParserView.h
@@ -73,6 +73,7 @@ public:
   /**
   * Token: get a unic threadId to fill safely les_var_view
   */
+#pragma clang optimize off
   KOKKOS_INLINE_FUNCTION int acquire() const { return token.acquire(); }
 #pragma clang optimize on
   KOKKOS_INLINE_FUNCTION void release(int threadId) const { token.release(threadId); }
