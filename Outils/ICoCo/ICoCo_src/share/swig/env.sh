2b54b179f86bb9f8777bbc7015e788afd11f06df
Elie Saikali <elie.saikali@cea.fr>
2026-03-13 20:59:57 +0100

    [Mac] ICoCo python cant find on Darwin libparaconf from libPDI ...

--- Outils/ICoCo/ICoCo_src/share/swig/env.sh
+++ Outils/ICoCo/ICoCo_src/share/swig/env.sh
@@ -6,3 +6,9 @@
 export PYTHONPATH=$TRUST_ROOT/Outils/ICoCo/ICoCo_src/share/swig/install/lib:$PYTHONPATH
 #export LD_LIBRARY_PATH=$TRUST_ROOT/exec/python/lib:$LD_LIBRARY_PATH  # Not needed anymore ?
 
+# if with PDI, we need libparaconf.1.dylib ... not from python but from pdi !
+if [[ "$TRUST_PDI_ROOT" != "" && "$(uname)" == "Darwin" ]]; then
+	if [[ "`echo $DYLD_LIBRARY_PATH | grep -i LIBPDI/lib`" = ""  ]]; then
+		export DYLD_LIBRARY_PATH=$TRUST_PDI_ROOT/lib:$DYLD_LIBRARY_PATH
+	fi
+fi
