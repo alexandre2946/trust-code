48cb29e597dced6bb6f6505ec854eaefe115ec0f
triou <trust@cea.fr>
2026-04-08 11:58:46 +0200

    [TPPI] fix valgrind issue

--- src/ThHyd/ThermoPhysical_properties_interfaces/EOS_to_TRUST/EOS_to_TRUST.h
+++ src/ThHyd/ThermoPhysical_properties_interfaces/EOS_to_TRUST/EOS_to_TRUST.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2024, CEA
+* Copyright (c) 2026, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -40,7 +40,12 @@ namespace NEPTUNE { class EOS; }
 class EOS_to_TRUST : public TPPI
 {
 public :
-  ~EOS_to_TRUST() override { /* delete fluide; */ }
+  ~EOS_to_TRUST() override
+  {
+#ifdef HAS_EOS
+    if (fluide) delete fluide;
+#endif
+  }
 
   void desactivate_handler(bool op = true) override;
 
