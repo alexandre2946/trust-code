9d11951a191a5e89e19f44fba182a6550bac4aff
Adrien BRUNETON <adrien.bruneton@cea.fr>
2026-04-27 13:52:18 +0200

    [trustify] Unit tests also checked when runing trustify

--- Outils/trustify/env.sh
+++ Outils/trustify/env.sh
@@ -7,8 +7,5 @@ root_dir=$TRUST_ROOT/Outils/trustify/install
 py_vers=`python -c "import sys; print('%d.%d' % (sys.version_info.major, sys.version_info.minor))"` 
 
 export TRUSTIFY_ROOT_DIR=${root_dir}/lib/python${py_vers}/site-packages
-export PYTHONPATH=${PYTHONPATH}:${TRUSTIFY_ROOT_DIR}
-
-# https://groups.google.com/g/qtcontribs/c/cWQgcrbKUCM useful to avoid QXcbConnection: XCB error: 3
-# export QT_LOGGING_RULES="*.debug=false"  
+export PYTHONPATH=${TRUSTIFY_ROOT_DIR}:${PYTHONPATH}
 
