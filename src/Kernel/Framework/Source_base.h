3427fdc1aa0795ec53ec0d4cd6e42b02ad3b2fcd
Teo BOUTIN <teo.boutin@cea.fr>
2026-05-06 16:19:20 +0200

    discretise source_base for override

--- src/Kernel/Framework/Source_base.h
+++ src/Kernel/Framework/Source_base.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2025, CEA
+* Copyright (c) 2026, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -57,6 +57,7 @@ public:
   // temporaire : associer_domaines sera rendue publique
   inline void associer_domaines_public(const Domaine_dis_base& zdis, const Domaine_Cl_dis_base& zcldis) { associer_domaines(zdis,zcldis); }
   virtual int initialiser(double temps);
+  virtual void discretiser() {}
   virtual void associer_champ_rho(const Champ_base& champ_rho);
   virtual int a_pour_Champ_Fonc(const Motcle& mot, OBS_PTR(Champ_base) &ch_ref) const;
   virtual void contribuer_jacobienne(Matrice_Bloc&, int) const { }
